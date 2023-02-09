11513e89bb946ebd8fa0532af7a06ac0c58a2c5a
Sefa GÜNDÜZ <sefagunduz@outlook.com.tr>
2023-02-09 14:56:53 +0300

    proje tamamlandı

--- DepremVerileri/USGS.cs
+++ DepremVerileri/USGS.cs
@@ -37,6 +37,36 @@ namespace DepremVerileri
         public Properties properties { get; set; }
         public Geometry geometry { get; set; }
         public string id { get; set; }
+
+        public class Properties
+        {
+            public double mag { get; set; }
+            public string place { get; set; }
+            public object time { get; set; }
+            public object updated { get; set; }
+            public object tz { get; set; }
+            public string url { get; set; }
+            public string detail { get; set; }
+            public int? felt { get; set; }
+            public double? cdi { get; set; }
+            public double? mmi { get; set; }
+            public string alert { get; set; }
+            public string status { get; set; }
+            public int tsunami { get; set; }
+            public int sig { get; set; }
+            public string net { get; set; }
+            public string code { get; set; }
+            public string ids { get; set; }
+            public string sources { get; set; }
+            public string types { get; set; }
+            public int? nst { get; set; }
+            public double? dmin { get; set; }
+            public double rms { get; set; }
+            public double? gap { get; set; }
+            public string magType { get; set; }
+            public string type { get; set; }
+            public string title { get; set; }
+        }
     }
 
     public class Geometry
@@ -55,42 +85,13 @@ namespace DepremVerileri
         public int count { get; set; }
     }
 
-    public class Properties
-    {
-        public double mag { get; set; }
-        public string place { get; set; }
-        public object time { get; set; }
-        public object updated { get; set; }
-        public object tz { get; set; }
-        public string url { get; set; }
-        public string detail { get; set; }
-        public int? felt { get; set; }
-        public double? cdi { get; set; }
-        public double? mmi { get; set; }
-        public string alert { get; set; }
-        public string status { get; set; }
-        public int tsunami { get; set; }
-        public int sig { get; set; }
-        public string net { get; set; }
-        public string code { get; set; }
-        public string ids { get; set; }
-        public string sources { get; set; }
-        public string types { get; set; }
-        public int? nst { get; set; }
-        public double? dmin { get; set; }
-        public double rms { get; set; }
-        public double? gap { get; set; }
-        public string magType { get; set; }
-        public string type { get; set; }
-        public string title { get; set; }
-    }
-
     public class Root
     {
         public string type { get; set; }
         public Metadata metadata { get; set; }
         public List<Feature> features { get; set; }
         public List<double> bbox { get; set; }
+
     }
 
 
--- README.md
+++ README.md
@@ -6,3 +6,46 @@ Kandilli Rasathanesi verilerini siteden çekip parse eder
 Afad verilerini siteden çekip okur
 
 USGS verilerini API üzerinden çekip Turkey kelimesi ile son depremlere göre filtreler.
+
+```
+using DepremVerileri;
+
+/* Afad üzerinden verileri çekme */
+
+List<DepremData> liste1 = Afad.SonVeriler();
+Console.WriteLine("---------------------------------------------------------");
+foreach (DepremData item in liste1)
+{
+    Console.WriteLine("ID:{0} - Tarih:{1} - Enlem:{2} - Boylam:{3} - Derinlik(KM):{4} - Tip:{5} - Büyüklük:{6} - Yer:{7} "
+        , item.ID, item.TarihSaat, item.Enlem, item.Boylam, item.DerinlikKM, item.Tip, item.Buyukluk, item.Yer);
+}
+
+/* Kandilli üzerinden verileri çekme */
+
+List<DepremData> liste2 = Kandilli.SonVeriler();
+Console.WriteLine("---------------------------------------------------------");
+foreach (DepremData item in liste2)
+{
+    Console.WriteLine("ID:{0} - Tarih:{1} - Enlem:{2} - Boylam:{3} - Derinlik(KM):{4} - Tip:{5} - Büyüklük:{6} - Yer:{7} "
+        , item.ID, item.TarihSaat, item.Enlem, item.Boylam, item.DerinlikKM, item.Tip, item.Buyukluk, item.Yer);
+}
+
+
+/* USGS üzerinden verileri çekme */
+
+List<Feature> liste3 = USGS.SonVeriler();
+Console.WriteLine("---------------------------------------------------------");
+foreach (Feature item in liste3)
+{
+    Console.WriteLine("ID:{0} - Tarih:{1} - Enlem:{2} - Boylam:{3} - Derinlik(KM):{4} - Tip:{5} - Büyüklük:{6} - Yer:{7} "
+        , item.id,
+        DateTimeOffset.FromUnixTimeMilliseconds((long)item.properties.time).UtcDateTime, 
+        item.geometry.coordinates[0], 
+        item.geometry.coordinates[1], 
+        "-",
+        item.properties.magType, item.properties.mag, item.properties.place);
+}
+
+Console.WriteLine("---------------------------------------------------------");
+
+```
